67d4e989f796cf312d78e891f915c99e4578b89b
Regnar <brian.mccrillis@gmail.com>
2020-02-01 21:39:55 -0500

    initial attempt for new images and cached API

--- main.py
+++ main.py
@@ -1,15 +1,35 @@
 import flask
 from flask import render_template, redirect, url_for, request
 import requests
+import threading
+import json
 
 app = flask.Flask(__name__)
+package = {}
+start = True
+def refresh_blocks():
+    global node, block, notarize, start
+    threading.Timer(300.0, refresh_blocks).start()
+    package['nodes'] = requests.get("https://komodostats.com/api/coin/nodecount.json").json()['count']
+    data = requests.get("http://kmd.explorer.dexstats.info/insight-api-komodo/status").json()['info']
+    package['blocks'] = data['blocks']
+    package['notarized'] = data['notarized']
+    start = False
+    with open('data.json', 'w') as f:
+        json.dump(package, f)
+    return print('refreshed')
+
+if start is True:
+    refresh_blocks()
 
 @app.route('/', methods=['GET'])
 def get_node_count():
-    nodes = requests.get("https://komodostats.com/api/coin/nodecount.json").json()['count']
-    data = requests.get("http://kmd.explorer.dexstats.info/insight-api-komodo/status").json()['info']
-    blocks = data['blocks']
-    notarized = data['notarized']
+    with open('data.json', 'r') as myfile:
+        data = myfile.read()
+    obj = json.loads(data)
+    blocks = obj['blocks']
+    notarized = obj['notarized']
+    nodes = obj['nodes']
     return render_template('admin.html', var1=blocks, var2=notarized, var3=nodes)
 
 
